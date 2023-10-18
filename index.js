7b56d2ebddace688b226573dd152bb45f9394556
Christina Larsen <clarsen1782@outlook.com>
2023-10-18 14:55:17 -0700

    2nd Psueo Code

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 // TODO: Create an array of questions for user input
 const questions = [];
+//Add type, name, and message for each question
+//add questions: added sections (discription, usage, license, ect)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+//Use FS module to write data and file 
 
 // TODO: Create a function to initialize app
 function init() {}
