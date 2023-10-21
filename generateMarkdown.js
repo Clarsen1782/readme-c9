8b8b1dc353f4a4620a8d7b9afe74534701da635a
Christina Larsen <clarsen1782@outlook.com>
2023-10-20 19:47:36 -0700

    Break on License

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,24 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  let badge = ``;
-  if (license === 'MIT') {
-    badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else {
-    badge = ``;
-  }
-}
+function renderLicenseBadge(license, link) {
+  const licenseBadge = {
+    'MIT': {
+      label: 'MIT License',
+      imgSrc: `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+    },
+    'Apache-2.0': {
+      label: 'Apache License 2.0',
+      imgSrc: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    },
+    'ISC': {
+      label: 'ISC License',
+      imgSrc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    },
+  };};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  let link = ``;
-  if (link === 'MIT') {
-    link = 'https://choosealicense.com/licenses/mit/';
+function renderLicenseLink(license, link) {
+  if (license && link) {
+    return `<a href="${licenseLink}" target="_blank">${licenseType}</a>`;
   } else {
-    link = ``;
+    return 'No License Information Provided';
   }
-
 }
 
 // TODO: Create a function that returns the license section of README
@@ -32,9 +41,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`;
 }
 
 module.exports = generateMarkdown;
