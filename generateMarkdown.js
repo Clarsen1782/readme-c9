94ac9ad5f136dbb8f8856f33dec6dc897bbec2d9
Christina Larsen <clarsen1782@outlook.com>
2023-10-20 21:27:45 -0700

    redo

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,47 +1,20 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license, link) {
-  const licenseBadge = {
-    'MIT': {
-      label: 'MIT License',
-      imgSrc: `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
-    },
-    'Apache-2.0': {
-      label: 'Apache License 2.0',
-      imgSrc: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
-    },
-    'ISC': {
-      label: 'ISC License',
-      imgSrc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
-    },
-  };};
-
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license, link) {
-  if (license && link) {
-    return `<a href="${licenseLink}" target="_blank">${licenseType}</a>`;
-  } else {
-    return 'No License Information Provided';
-  }
-}
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
-// creat a conditional to check if license exists
-// Hint - call renderlicense function inside
-// Call render badge & link
-// return template literal for format
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
+  return `# ${data.title}
+
+`;
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,96 +1,13 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the Title?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'What is the description?',
-        },
-        {
-            type: 'input',
-            name: 'install',
-            message: 'How is the application installed?',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'How should the application be used?',
-        },
-        {
-            type: 'input',
-            name: 'con1',
-            message: 'Where there any collaborators?',
-        },
-        {
-            type: 'input',
-            name: 'con2',
-            message: 'Where there any third-party assets?',
-        },
-        {
-            type: 'input',
-            name: 'con3',
-            message: 'Did you follow any tutorials?',
-        },
-        {
-            type: 'checkbox',
-            name: 'license',
-            message: 'Check which License',
-            choices: ['MIT', 'NONE'],
-            validate: (generateMarkdown) => {
-                if (generateMarkdown) {
-                    return true;
-                }
-            }
-        },
-    ])
-    .then((answers) => {
-        const readmePageContent = generateREADME(answers);
-        fs.writeFile("README.md", readmePageContent, (err) =>
-        err ? console.log(err) : console.log('Seccess')
-        );
-    });
-    const generateREADME = (answers) =>
-    `# ${answers.title}
-  
-    ## Description
-    ${answers.description}
-    
-    ## Installation
-    ${answers.install}
-    
-    ## Usage
-    ${answers.usage}
-    
-    
-    ## Contributing
-    ${answers.con1}
-    ${answers.con2}
-    ${answers.con3}
-    
-    ## License
-    ${answers.license}`;
-
-
-    /*const questions = [];
-//Add type, name, and message for each question
-//add questions: added sections (discription, usage, license, ect)
+const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-//Use FS module to write data and file 
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();*/
+init();
