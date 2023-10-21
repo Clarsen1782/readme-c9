9e3f9b703b9c78ad38c96a79f85c3940a3c8342d
Christina Larsen <clarsen1782@outlook.com>
2023-10-20 22:52:53 -0700

    Maybe I have it

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,51 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'none') {
+    // Use encodeURIComponent to properly encode the license value in the URL
+    const encodedLicense = encodeURIComponent(license);
+    return `![GitHub license](https://img.shields.io/badge/License-${encodedLicense}-yellow.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function generateLicenseLink() {
+  if (license !== 'none') {
+    return '\n* [License](#license)\n';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `## License
+
+Licensed under the ${license} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+    ## Description
+    ${data.description}
+    ## Installation
+    ${data.install}
+    ## Usage
+    ${data.used}
+    ## Contributing
+    ${data.collaborators}
+    ${data.third-party}
+    ${data.tutorials}
+    ## License
+    ${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        question: 'What is the Title?',
+        variable: 'title',
+    },
+    {
+        question: 'What is the description?',
+        variable: 'description',
+    },
+    {
+        question: 'How is the application installed?',
+        variable: 'installed',
+    },
+    {
+        question: 'How should the application be used?',
+        variable: 'used',
+    },
+    {
+        question: 'Where there any collaborators?',
+        variable: 'collaborators',
+    },
+    {
+        question: 'Where there any third-party assets?',
+        variable: 'third-party',
+    },
+    {
+        question: 'Did you follow any tutorials?',
+        variable: 'tutorials',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(questions) {
+    return fs.writeToFileSync(path.join(process.cwd(), questions));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log('Creating');
+        writeToFile('./readme.md', generateMarkdown(data.title))
+    })
+}
 
 // Function call to initialize app
 init();
